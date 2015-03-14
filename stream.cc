a70d5b575515b85ad8c9631efc58f3a79aa3df9c
Oleg Sidorkin <oleg.sidorkin@gmail.com>
2015-03-13 23:52:46 -0700

    little cleanup for stream implementation

--- stream.cc
+++ stream.cc
@@ -3,24 +3,26 @@
 
 using namespace std;
 
+const size_t stream_t::chunkSize = 10*1024*1024;
+
 stream_t::stream_t(istream& i) : in(i), chunkBuffer(0), chunkRemain(0) {}
 
 const char* stream_t::next() {
     char* p = 0;
-    if (getline(in, s)) {
-        if (chunkRemain <= s.size() && s.size() < chunkSize) {
+    if (getline(in, tmp)) {
+        if (chunkRemain <= tmp.size() && tmp.size() < chunkSize) {
             chunkBuffer = new char[chunkSize];
             chunkRemain = chunkSize;
         }
-        if (chunkRemain > s.size()) {
+        if (chunkRemain > tmp.size()) {
             p = chunkBuffer;
-            chunkRemain -= s.size() + 1;
-            chunkBuffer += s.size() + 1;
+            chunkRemain -= tmp.size() + 1;
+            chunkBuffer += tmp.size() + 1;
         }
         else
-            p = new char[s.size() + 1];
-        memcpy(p, s.data(), s.size());
-        p[s.size()] = 0;
+            p = new char[tmp.size() + 1];
+        memcpy(p, tmp.data(), tmp.size());
+        p[tmp.size()] = 0;
     }
     return p;
 }
--- stream.h
+++ stream.h
@@ -3,11 +3,11 @@
 
 struct stream_t {
 	std::istream& in;
-	static const size_t chunkSize = 10*1024*1024;
+	static const size_t chunkSize;
 	char* chunkBuffer;
 	size_t chunkRemain;
-	std::string s;
-	stream_t(std::istream&);
+	std::string tmp;
 
+	stream_t(std::istream&);
 	const char* next();
 };
